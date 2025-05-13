0a24cf13fc004dc7c2ee44329e164cde0ca1313e
nottworry1 <kupchenkoanastasiia@gmail.com>
2025-05-13 18:06:43 +0200

    initial commit

---
+++ src/main/java/org/kupchnko/projectmanagementllm/ProjectManagementLlmApplication.java
@@ -0,0 +1,15 @@
+package org.kupchnko.projectmanagementllm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementLlmApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ProjectManagementLlmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kupchnko/projectmanagementllm/ProjectManagementLlmApplicationTests.java
@@ -0,0 +1,15 @@
+package org.kupchnko.projectmanagementllm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementLlmApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
